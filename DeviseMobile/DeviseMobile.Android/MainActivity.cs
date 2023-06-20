b862035946bc1f4150619f8d4e8b8ea4fde0dcd8
xHamelion <daniilelushov@mail.ru>
2023-06-20 20:32:56 +0300

    Fixed2

--- DeviseMobile/DeviseMobile.Android/MainActivity.cs
+++ DeviseMobile/DeviseMobile.Android/MainActivity.cs
@@ -7,7 +7,7 @@ using Android.OS;
 
 namespace DeviseMobile.Droid
 {
-    [Activity(Label = "Девайс Mobile", ScreenOrientation = Android.Content.PM.ScreenOrientation.Locked, Icon = "@mipmap/ic_launcher", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "Девайс", ScreenOrientation = Android.Content.PM.ScreenOrientation.Locked, Icon = "@mipmap/ic_launcher", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
